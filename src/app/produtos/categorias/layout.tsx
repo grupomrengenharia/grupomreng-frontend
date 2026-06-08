403a586929769e0c830cd7b8cd67b454717a52ba
Pedro Braghin <braghindev@gmail.com>
2026-06-08 18:08:46 -0300

    fix(route): remove invalid params

--- src/app/produtos/categorias/layout.tsx
+++ src/app/produtos/categorias/layout.tsx
@@ -1,18 +1,12 @@
 import { SideMenu } from '../components/side-menu';
 import categories from '@/data/categories.json';
 import { CategoryTree } from '../components/types';
-import { normalizeCategoryName } from '@/src/utils';
 
-export default async function ProductsPage({
+export default function ProductsPage({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ slug: string }>;
 }>) {
-  const resolvedParams = await params;
-  console.log('Resolved params in layout:', resolvedParams);
-
   return (
     <section className="flex flex-col gap-10 section min-h-[80vh] pt-10 pb-10">
       <div className="flex gap-10 flex-col lg:flex-row">
